7a61b5c3cd9b4aa440ce0f87af8dc026458c292c
anchenayres <21100284@virtualwindow.co.za>
2023-03-26 13:39:26 +0200

    Models
    Models folder created with a item.ts file to export the data

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent {
+  america=[
+    {
+      image: "https://cdn.shopify.com/s/files/1/0280/5548/products/Legado-Kenya-Kiamabara-Peaberry-250g_1024x1024.jpg?v=1664978064",
+      title: "Cedar - Colombia Felix Samboni Natural",
+      price: "R205,87",
+      size: "250g Total",
+      availability: "1096g",
+  },
+  {
+    image: "https://cdn.shopify.com/s/files/1/0280/5548/products/Truth-White-Bag-With-Clip-1024x1024_ecef9fd0-e305-4e5e-b64f-e71400c873fc_1024x1024.jpg?v=1665661221",
+    title: "Cedar - Colombia Felix Samboni Washed",
+    price: "R205,87",
+    size: "250g Total",
+    availability: "1096g",
+},
+  {
+    image: "https://cdn.shopify.com/s/files/1/0280/5548/products/Legado-Kenya-Kiamabara-Peaberry-250g_1024x1024.jpg?v=1664978064",
+    title: "Cedar - Nicaragua San Roman",
+    price: "R205,87",
+    size: "250g Total",
+    availability: "1096g",
+}
+]
 
 }
