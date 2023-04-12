86d8ef0feff536fa619c12406e669f28f00027a4
anchenayres <21100284@virtualwindow.co.za>
2023-04-12 13:02:20 +0200

    error serive

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,17 @@ import { Injectable } from '@angular/core';
 export class ErrorService {
 
   constructor() { }
+
+  public handleError(error: HttpErrorResponse) {
+    //0 means client side error
+    if(error.status === 0) {
+      console.log("An error occured: ", error.error);
+    } else {
+      //backend side error
+      console.log(`Backend returned code ${error.status}, body was: `, error.error)
+    }
+
+    //return an observable wuth user-facing error message
+    return throwError(() => new Error('Something bad happened; please try again later'))
+  }
 }
--- src/app/services/verify.service.ts
+++ src/app/services/verify.service.ts
@@ -1,18 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, retry } from 'rxjs';
+import { ErrorService } from './error.service';
+import {throwError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VerifyService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private errorHandler: ErrorService) { }
 
   httpUrl: string = "http://localhost:3000/user/login";
 
   //method to check if user is verified to craft
   checkVerification( username: string, password: string): Observable<any> {
     return this.http.post<any>(this.httpUrl,{ username, password })
+    .pipe(
+      retry(2),
+      catchError(this.errorHandler.handleError)
+    )
   }
+
+  private handleError(error: HttpErrorResponse) {
+    //0 means client side error
+    if(error.status === 0) {
+      console.log("An error occured: ", error.error);
+    } else {
+      //backend side error
+      console.log(`Backend returned code ${error.status}, body was: `, error.error)
+    }
+
+    //return an observable wuth user-facing error message
+    return throwError(() => new Error('Something bad happened; please try again later'))
+  }
+
 }
